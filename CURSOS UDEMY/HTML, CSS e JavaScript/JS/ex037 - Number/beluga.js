ae18586af334d4bb8b8fff7b18a139f970f44664
LucasCr <lucas.crr95@gmail.com>
2022-02-13 23:14:57 -0300

    20220213
    Estudo JS

---
+++ CURSOS UDEMY/HTML, CSS e JavaScript/JS/ex037 - Number/beluga.js
@@ -0,0 +1,12 @@
+function calcularMedia(){
+    var result = 0;
+    var x = 0;
+
+    while(x < arguments.length){
+        result += arguments[x];
+        x++;
+    }
+    result /= arguments.length; 
+    
+    return result.toFixed(2);
+}
