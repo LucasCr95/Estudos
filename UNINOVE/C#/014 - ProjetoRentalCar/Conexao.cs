16db554b9f83b39c23138a1312482b1a322a6435
LucasCruz <lucas.crr95@gmail.com>
2022-06-03 01:52:22 -0300

    20220603
    Estudos C# - #DML #DQL #TiposDeDados #OperadoresSQL #MaskedTextBox

--- UNINOVE/C#/014 - ProjetoRentalCar/Conexao.cs
+++ UNINOVE/C#/014 - ProjetoRentalCar/Conexao.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//Definições dos namespace usado no projeto para o SQL Server
+using System.Data;
+using System.Data.SqlClient;
 
 namespace _014___ProjetoRentalCar
 {
-    class Conexao
+    public class Conexao
     {
+        //método para relizar a conexão com o DB
+        public static SqlConnection getConnection()
+        {
+            //cria um objeto do tipo SqlConennection e configura a string de conexão
+
+            //Adicionando o sinal de @ antes das aspas dupla
+            SqlConnection cnn = new SqlConnection(@"Data Source=DESKTOP-V03RTFN\DB_SQLCR;Initial Catalog=DB_RENTALCAR;Integrated Security=True;Pooling=False");
+            return cnn;
+        }
     }
 }
---
+++ UNINOVE/C#/014 - ProjetoRentalCar/frmCadastroCliente.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace _014___ProjetoRentalCar
+{
+    partial class frmCadastroCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.mskTxtCPF = new System.Windows.Forms.MaskedTextBox();
+            this.lblDtNasc = new System.Windows.Forms.Label();
+            this.mskTxtNasc = new System.Windows.Forms.MaskedTextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnIncluir = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(33, 37);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(43, 13);
+            this.lblCodigo.TabIndex = 0;
+            this.lblCodigo.Text = "Código:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(36, 64);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.Location = new System.Drawing.Point(198, 37);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(27, 13);
+            this.lblCPF.TabIndex = 3;
+            this.lblCPF.Text = "CPF";
+            this.lblCPF.Click += new System.EventHandler(this.lblCPF_Click);
+            // 
+            // mskTxtCPF
+            // 
+            this.mskTxtCPF.Location = new System.Drawing.Point(201, 64);
+            this.mskTxtCPF.Mask = "999.999.999-99";
+            this.mskTxtCPF.Name = "mskTxtCPF";
+            this.mskTxtCPF.Size = new System.Drawing.Size(100, 20);
+            this.mskTxtCPF.TabIndex = 4;
+            // 
+            // lblDtNasc
+            // 
+            this.lblDtNasc.AutoSize = true;
+            this.lblDtNasc.Location = new System.Drawing.Point(359, 37);
+            this.lblDtNasc.Name = "lblDtNasc";
+            this.lblDtNasc.Size = new System.Drawing.Size(104, 13);
+            this.lblDtNasc.TabIndex = 5;
+            this.lblDtNasc.Text = "Data de Nascimento";
+            // 
+            // mskTxtNasc
+            // 
+            this.mskTxtNasc.Location = new System.Drawing.Point(362, 64);
+            this.mskTxtNasc.Mask = "00/00/0000";
+            this.mskTxtNasc.Name = "mskTxtNasc";
+            this.mskTxtNasc.Size = new System.Drawing.Size(83, 20);
+            this.mskTxtNasc.TabIndex = 6;
+            this.mskTxtNasc.TextMaskFormat = System.Windows.Forms.MaskFormat.IncludePromptAndLiterals;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(36, 121);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 7;
+            this.lblNome.Text = "Nome";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(39, 151);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(424, 20);
+            this.txtNome.TabIndex = 8;
+            // 
+            // btnIncluir
+            // 
+            this.btnIncluir.Location = new System.Drawing.Point(39, 201);
+            this.btnIncluir.Name = "btnIncluir";
+            this.btnIncluir.Size = new System.Drawing.Size(75, 23);
+            this.btnIncluir.TabIndex = 9;
+            this.btnIncluir.Text = "Incluir";
+            this.btnIncluir.UseVisualStyleBackColor = true;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(39, 247);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 10;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(213, 247);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 11;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(213, 201);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.TabIndex = 12;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(388, 201);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 13;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            // 
+            // frmCadastroCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 328);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnIncluir);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.mskTxtNasc);
+            this.Controls.Add(this.lblDtNasc);
+            this.Controls.Add(this.mskTxtCPF);
+            this.Controls.Add(this.lblCPF);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.lblCodigo);
+            this.Name = "frmCadastroCliente";
+            this.Text = "Cadastro Cliente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.MaskedTextBox mskTxtCPF;
+        private System.Windows.Forms.Label lblDtNasc;
+        private System.Windows.Forms.MaskedTextBox mskTxtNasc;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnIncluir;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnFechar;
+    }
+}
